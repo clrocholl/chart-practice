370912d01be604b6d6f9435e754cb16a3c20becc
clrocholl <claudia.g.rocholl@gmail.com>
2018-07-30 21:18:22 -0500

    add another chart to the page

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 // Load the Visualization API and the corechart package.
-google.charts.load('current', {
-  'packages': ['corechart']
-});
-
-// Set a callback to run when the Google Visualization API is loaded.
+google.charts.load('current', {'packages': ['corechart']});
 google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawAllocationChart);
 
-// Callback that creates and populates a data table,
-// instantiates the pie chart, passes in the data and
-// draws it.
 function drawChart() {
-
-  // Create the data table.
   var data = new google.visualization.DataTable();
   data.addColumn('string', 'Score Range');
   data.addColumn('number', 'Games');
@@ -21,14 +13,32 @@ function drawChart() {
     ['200-300', 3]
   ]);
 
-  // Set chart options
   var options = {
     'title': 'Bowling Score Incidences',
     'width': 400,
     'height': 300
   };
 
-  // Instantiate and draw our chart, passing in some options.
   var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
+}
+
+function drawAllocationChart() {
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Score Range');
+  data.addColumn('number', 'Games');
+  data.addRows([
+    ['0-100', 2],
+    ['100-200', 5],
+    ['200-300', 3]
+  ]);
+
+  var options = {
+    'title': 'Bowling Score Incidences',
+    'width': 400,
+    'height': 300
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('allocations'));
   chart.draw(data, options);
 }
